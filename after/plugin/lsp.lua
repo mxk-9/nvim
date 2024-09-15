8817b23afc7a0a17628a304d71f3e8bab4e5cf78
Sny Spyper <snyspyper@proton.me>
2024-09-16 00:21:34 +0300

    Add org-mode and support for zig, powershell

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,33 +2,33 @@ local lsp_zero = require('lsp-zero')
 local lua_opts = lsp_zero.nvim_lua_ls()
 
 local lsp_format_on_save = function(bufnr)
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    group = augroup,
-    buffer = bubfnr,
-    callback = function()
-      vim.lsp.buf.format()
-    end,
-  })
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	vim.api.nvim_create_autocmd('BufWritePre', {
+		group = augroup,
+		buffer = bubfnr,
+		callback = function()
+			vim.lsp.buf.format()
+		end,
+	})
 end
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  lsp_zero.default_keymaps({ buffer = bufnr })
-  lsp_format_on_save(bufnr)
+	-- see :help lsp-zero-keybindings
+	lsp_zero.default_keymaps({ buffer = bufnr })
+	lsp_format_on_save(bufnr)
 end)
 
 -- Нормальный вывод ошибок
 vim.diagnostic.config({
-  virtual_text = true,
-  severoty_sort = true,
-  float = {
-    style  = 'minimal',
-    border = 'rounded',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
+	virtual_text = true,
+	severoty_sort = true,
+	float = {
+		style  = 'minimal',
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	},
 })
 
 -- here you can setup the language servers
@@ -40,6 +40,7 @@ require 'lspconfig'.lua_ls.setup(lua_opts)
 require 'lspconfig'.pylsp.setup {}
 require 'lspconfig'.rust_analyzer.setup {}
 require 'lspconfig'.jsonnet_ls.setup {}
+require 'lspconfig'.zls.setup {}
 
 require 'lspconfig'.nixd.setup {}
 -- --- @type { name: string, system: string, flakePath: string }
