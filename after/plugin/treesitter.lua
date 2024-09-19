37e565a97e71079234d4ee606faab128a51eac99
Sny Spyper <snuspuper@example.com>
2024-09-19 17:29:26 +0300

    there is no powershell grammar for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "nu", "vimdoc", "rust", "go", "c", "lua", "vim", "vimdoc", "query", "json", "javascript", "nix", "powershell", "zig" },
+	ensure_installed = { "nu", "vimdoc", "rust", "go", "c", "lua", "vim", "vimdoc", "query", "json", "javascript", "nix", "zig" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
